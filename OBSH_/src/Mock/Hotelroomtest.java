2ed7093d146a6cf2e7e9fa60eaca327d8ca772cf
whiteCQC <530660508@qq.com>
2016-11-13 22:14:43 +0800

    6141

--- OBSH_/src/Mock/Hotelroomtest.java
+++ OBSH_/src/Mock/Hotelroomtest.java
@@ -9,45 +9,44 @@ import bl.HotelRoombl.HotelroomBLServiceImpl;
 
 public class Hotelroomtest {
 	@Test
-public void testAddroom(){
-	MockHotelroom hotel = new MockHotelroom (10,00001,"2016-09-09","2016-09-11","2016-09-12");
-	HotelroomBLService hotel1 = new HotelroomBLServiceImpl();
-	int room1=hotel1.Addroom(20);	
-	assertEquals(room1,30);
-}
-	@Test
-	public void testTimein(){
-	MockHotelroom hotel = new MockHotelroom (10,00001,"2016-09-09","2016-09-11","2016-09-12");
-	HotelroomBLService hotel1 = new HotelroomBLServiceImpl();
-	String datein=hotel1.Timein("2016-09-09");
-	assertEquals(datein,"2016-09-09");
+	public void testAddroom() {
+		MockHotelroom hotel = new MockHotelroom(10, 00001, "2016-09-09", "2016-09-11", "2016-09-12");
+		HotelroomBLService hotel1 = new HotelroomBLServiceImpl();
+		assertEquals(hotel.room, hotel1.Addroom(hotel.getroom()));
 	}
+
 	@Test
-	public void testIDin(){
-	MockHotelroom hotel = new MockHotelroom (10,00001,"2016-09-09","2016-09-11","2016-09-12");
-	HotelroomBLService hotel1 = new HotelroomBLServiceImpl();
-    int ID1=hotel1.IDin(00001);
-    assertEquals(ID1,00001);
+	public void testTimein() {
+		MockHotelroom hotel = new MockHotelroom(10, 00001, "2016-09-09", "2016-09-11", "2016-09-12");
+		HotelroomBLService hotel1 = new HotelroomBLServiceImpl();
+		assertEquals(hotel.datein, hotel1.Timein(hotel.datein));
 	}
+
 	@Test
-	public void testTimeoutpro(){
-	MockHotelroom hotel = new MockHotelroom (10,00001,"2016-09-09","2016-09-11","2016-09-12");
-	HotelroomBLService hotel1 = new HotelroomBLServiceImpl();
-	String dateoutpro = hotel1.TimeOutpro("2016-09-11");
-	assertEquals(dateoutpro,"2016-09-11");
+	public void testIDin() {
+		MockHotelroom hotel = new MockHotelroom(10, 00001, "2016-09-09", "2016-09-11", "2016-09-12");
+		HotelroomBLService hotel1 = new HotelroomBLServiceImpl();
+		assertEquals(hotel.id, hotel1.IDin(hotel.getid()));
 	}
+
 	@Test
-	public void testTimeout(){
-	MockHotelroom hotel = new MockHotelroom (10,00001,"2016-09-09","2016-09-11","2016-09-12");
-	HotelroomBLService hotel1 = new HotelroomBLServiceImpl();
-	String timeout = hotel1.Timeout("2016-09-12");
-	assertEquals(timeout,"2016-09-12");
+	public void testTimeoutpro() {
+		MockHotelroom hotel = new MockHotelroom(10, 00001, "2016-09-09", "2016-09-11", "2016-09-12");
+		HotelroomBLService hotel1 = new HotelroomBLServiceImpl();
+		assertEquals(hotel.dateoutpro, hotel1.TimeOutpro(hotel.getdateoutpro()));
 	}
+
 	@Test
-	public void testIDout(){
-	MockHotelroom hotel = new MockHotelroom (10,00001,"2016-09-09","2016-09-11","2016-09-12");
-	HotelroomBLService hotel1 = new HotelroomBLServiceImpl();
-	int ID2=hotel1.IDout(00001);
-	assertEquals(ID2,00001);
+	public void testTimeout() {
+		MockHotelroom hotel = new MockHotelroom(10, 00001, "2016-09-09", "2016-09-11", "2016-09-12");
+		HotelroomBLService hotel1 = new HotelroomBLServiceImpl();
+		assertEquals(hotel.timeout, hotel1.Timeout(hotel.gettimeout()));
+	}
+
+	@Test
+	public void testIDout() {
+		MockHotelroom hotel = new MockHotelroom(10, 00001, "2016-09-09", "2016-09-11", "2016-09-12");
+		HotelroomBLService hotel1 = new HotelroomBLServiceImpl();
+		assertEquals(hotel.id, hotel1.IDout(hotel.getid()));
 	}
 }
