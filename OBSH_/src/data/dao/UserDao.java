e04a443f4e2fd91e0425a34aad308599960e917d
Little Painter <397342673@qq.com>
2016-11-15 20:24:59 +0800

    更新userDao

--- OBSH_/src/data/dao/UserDao.java
+++ OBSH_/src/data/dao/UserDao.java
@@ -1,5 +1,22 @@
 package data.dao;
+/**
+ * @author bxh
+ */
+import po.UserPo;
 
-public class UserDao {
+public interface UserDao {
+	
+	/**
+	 * @param userId
+	 * @return	获取用户信息
+	 */
+	public UserPo getUser(int userId);
+	
+	/**
+	 * @param userPo
+	 * @return	更新用户信息
+	 */
+	public boolean updateUser(UserPo userPo);
 
 }
+
